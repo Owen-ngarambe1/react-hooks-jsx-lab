c58b9791960198bef286c92140146b087f650ca0
owen ngarambe <ngarambe.owen@student.moringaschool.com>
2024-05-06 05:41:03 +0300

    1

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { name, city } from "../data/data.js";
+
+function Home() {
+  return (
+    <div id="home">
+      <h1 style={{color: "firebrick"}}>{name} is a Web Developer from {city}</h1>
+      <h2>{name}</h2>
+      <p>{city}</p>
+    </div>
+    );
+  return <div>Home</div>;
+}
+
+export default Home;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+    );
+  return <nav>NavBar</nav>;
+}
+
+export default NavBar;
